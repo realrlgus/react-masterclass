6f5f3f9da474a8559496b3a34fa15b3641721b5c
kihyeon kwon <realrlgus@naver.com>
2021-12-18 23:08:02 +0900

    Make a Coin Website

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  backgroundColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#4cd137",
+  overViewColor: "hsl(198, 15%, 10%)",
+};
